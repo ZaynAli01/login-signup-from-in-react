75840a08881bcbfcc9c12b6ee6967a89fa29b4aa
Zain Ali <shzayn34@gmail.com>
2025-03-22 15:01:38 +0500

    ctreated sperate icon component

---
+++ src/component/Icons.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Icons() {
+  return (
+    <div className="socail-media-icons">
+      <i className="fa-brands fa-google"></i>
+      <i className="fa-brands fa-facebook-f"></i>
+      <i className="fa-brands fa-github"></i>
+      <i className="fa-brands fa-linkedin-in"></i>
+    </div>
+  )
+}
